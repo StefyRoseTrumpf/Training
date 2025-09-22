823b89edb5f384accbe3dabad0ddb03991b71229
StefyRoseTrumpf <AmalStefy.Regis@trumpf.com>
2025-09-22 14:36:19 +0530

    T02: Intial commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -9,16 +9,13 @@ internal class Program {
    static void Main (string[] args) {
       (int, string)[] romanSymbol = { (1000,"M"),(900,"CM"),(500,"D"),(400,"CD"),(100,"C"),(90,"XC"),(50,"LX"),(40,"XL"),
                                             (10,"X"),(9,"IX"),(5,"V"),(4,"IV"),(1,"I")};
-
       Console.WriteLine ("Enter a number");
       string input = Console.ReadLine ();
       int num = Convert.ToInt32 (input);
 
       while (true) {
          Console.WriteLine ($"Input:{num},\nConvert {num} to Roman or Words? (Enter roman/word)");
-
          string userChoice = Console.ReadLine ();
-
 
          if (userChoice.ToLower ().Trim () == "roman") {
 
@@ -33,34 +30,24 @@ internal class Program {
             Console.WriteLine ("Enter a valid choice - Roman or Word");
          }
       }
-
-
       static string ConvertToRoman (int number, (int, string)[] romanSymbol) {
-
          string output = "";
-
          foreach (var (value, symbol) in romanSymbol) {
             while (number >= value) {
                output += symbol;
                number -= value;
             }
-
          }
          return output;
-
       }
-
       static string ConvertToWords (int number) {
          if (number == 0) {
             return "Zero";
          }
-
          if (number < 0) {
             return "Minus" + ConvertToWords (Math.Abs (number));
          }
-
          string words = "";
-
          if ((number / 1000000) > 0) {
             words += ConvertToWords (number / 1000000) + " Million ";
             number %= 1000000;
@@ -77,11 +64,9 @@ internal class Program {
             words += ConvertToWords (number / 100) + " Hundred ";
             number %= 100;
          }
-
          if (number > 0) {
             string[] ones = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
             string[] tens = { "Zero", "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty" };
-
             if (number < 20) {
                words += ones[number];
             } else {
@@ -89,14 +74,10 @@ internal class Program {
                if ((number % 10) > 0) {
                   words += " " + ones[number % 10];
                }
-
             }
-
          }
-
          return words;
       }
-
       Console.ReadLine ();
    }
 }
