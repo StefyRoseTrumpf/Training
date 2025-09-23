b37cb8588704e587e8c761c0df477c1267a431d9
StefyRoseTrumpf <AmalStefy.Regis@trumpf.com>
2025-09-23 13:53:39 +0530

    T03: Intial Commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,11 +3,38 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T03 branch. Assignment Q3:LCM and GCD Generator
 // ------------------------------------------------------------------------------------------------
 namespace Training_25;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      Console.WriteLine ("Enter the first number:");
+      string? input1 = Console.ReadLine ();
+      int a;
+      while (!int.TryParse (input1, out a)) {
+         Console.WriteLine ("Invalid input.Please enter a valid number.");
+         input1 = Console.ReadLine ();
+      }
+      Console.WriteLine ("Enter the second number:");
+      string? input2 = Console.ReadLine ();
+      int b;
+      while (!int.TryParse (input2, out b)) {
+         Console.WriteLine ("Invalid input.Please enter a valid number.");
+         input2 = Console.ReadLine ();
+      }
+      Console.WriteLine ($"GCD:{gcd (a, b)}");
+      Console.WriteLine ($"LCM:{lcm (a, b)}");
+      static int gcd (int a, int b) {
+         while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+         }
+         return a;
+      }
+      static int lcm (int a, int b) {
+         return (a / gcd (a, b)) * b;
+      }
+      Console.ReadLine ();
    }
 }
