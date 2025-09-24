6074f59bdeaee7641f0e852f472e22a79f811674
StefyRoseTrumpf <AmalStefy.Regis@trumpf.com>
2025-09-24 12:10:45 +0530

    Revert "T03: Rectified" - Merge Conflicts
    This reverts commit 463f1e0686116111ecdbf09cfe8568726b652aaa.

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -9,7 +9,6 @@ using static System.Console;
 namespace Training_25;
 internal class Program {
    static void Main (string[] args) {
-<<<<<<< Updated upstream
       WriteLine ("Enter the first number:");
       int a;
       while (!int.TryParse (ReadLine (), out a))
@@ -26,59 +25,6 @@ internal class Program {
             int temp = b;
             b = a % b;
             a = temp;
-||||||| Stash base
-      Console.WriteLine ("Enter a number");
-      string? input = Console.ReadLine ();
-      int num;
-      while (string.IsNullOrWhiteSpace (input) || !int.TryParse (input, out num)) {
-         Console.WriteLine ("Invalid input. Please enter a  number:");
-         input = Console.ReadLine ();
-      }
-      Console.WriteLine ($"Input: {num}");
-      int n = num;
-      if (n == 0) Console.WriteLine ("HEX: 0");
-      else {
-         bool isNegative = n < 0;
-         n = Math.Abs (n);
-         StringBuilder sb = new ();
-         while (n > 0) {
-            int r = n % 16;
-            n = n / 16;
-            if (r < 10) 
-               sb.Insert (0, r);
-             else {
-               switch (r) {
-                  case 10: sb.Insert (0, "A"); break;
-                  case 11: sb.Insert (0, "B"); break;
-                  case 12: sb.Insert (0, "C"); break;
-                  case 13: sb.Insert (0, "D"); break;
-                  case 14: sb.Insert (0, "E"); break;
-                  case 15: sb.Insert (0, "F"); break;
-               }
-            }
-=======
-      Console.WriteLine ("Enter a number");
-      string? input = Console.ReadLine ();
-      int num;
-      while (string.IsNullOrWhiteSpace (input) || !int.TryParse (input, out num)) {
-         Console.WriteLine ("Invalid input. Please enter a  number:");
-         input = Console.ReadLine ();
-      }
-      Console.WriteLine ($"Input: {num}");
-      int n = num;
-      if (n == 0) Console.WriteLine ("HEX: 0");
-      else {
-         bool isNegative = n < 0;
-         n = Math.Abs (n);
-         StringBuilder sb = new ();
-         while (n > 0) {
-            int r = n % 16;
-            n = n / 16;
-            if (r < 10) 
-               sb.Insert (0, r);
-             else
-               sb.Insert (0, (char)('A' + r - 10));
->>>>>>> Stashed changes
          }
          return a;
       }
