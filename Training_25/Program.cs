950bb3397005670a41d8a611bf9f47156e0e6033
StefyRoseTrumpf <AmalStefy.Regis@trumpf.com>
2025-09-25 09:57:52 +0530

    T07: Initial commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,11 +3,28 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T07 branch. Assignment Q7: Pascal's Triangle.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training_25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      WriteLine ("Enter the number of rows for the Pascal's triangle:");
+      int rows;
+      while (true) {
+         if (int.TryParse (ReadLine (), out rows) && rows >= 0) break;
+         WriteLine ("Invalid input. Please enter a valid number (whole numbers).");
+      }
+      for (int i = 0; i < rows; i++) {
+         Write (new string (' ', (rows - i) * 2));
+         int value = 1;
+         for (int j = 0; j <= i; j++) {
+            Write ($"{value,4}");
+            value = value * (i - j) / (j + 1);
+         }
+         WriteLine ();
+      }
+      ReadLine ();
    }
 }
