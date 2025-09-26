48a25d3c9f7b9fb29f424ee10eeddcfe5cab4c46
StefyRoseTrumpf <AmalStefy.Regis@trumpf.com>
2025-09-26 10:49:13 +0530

    T10: Initial commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,11 +3,38 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T10 branch. Assignment Q10: Reverse the String
 // ------------------------------------------------------------------------------------------------
+using System.Text;
+using static System.Console;
 namespace Training_25;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      WriteLine ("Enter a string:");
+      string? input = ReadLine ();
+      string sb = StringReverser (input);
+      WriteLine ($"Input string: {input}\nReversed string: {sb}");
+
+      static string StringReverser (string? input) {
+         if (string.IsNullOrEmpty (input)) return string.Empty;
+         var letters = new StringBuilder ();
+         foreach (char c in input) {
+            if (c != ' ') letters.Append (c);
+         }
+         char[] reversedLetters = letters.ToString ().ToCharArray ();
+         Array.Reverse (reversedLetters);
+         var sb = new StringBuilder ();
+         int letterIndex = 0;
+         foreach (char c in input) {
+            if (c == ' ') sb.Append (' ');
+            else {
+               char ch = reversedLetters[letterIndex++];
+               sb.Append (char.IsUpper (c) ? char.ToUpper (ch) : char.ToLower (ch));
+            }
+         }
+         return sb.ToString ();
+      }
+      ReadLine ();
    }
 }
