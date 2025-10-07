88f3ad07ccf24bfa77c07bcc68e3ba4200aa1b8d
stefyrose <AmalStefy.Regis@trumpf.com>
2025-10-07 14:24:09 +0530

    T05: Rectified

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,19 +3,17 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T05 branch. Assignment Q5: Multiplication Table
+// Assignment 5: Multiplication Table
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training_25;
 
 internal class Program {
-   static void Main (string[] args) {
-      WriteLine ("Enter a number, to display it's multiplication table.");
-      int num;
-      while (!int.TryParse (ReadLine (), out num))
-         WriteLine ("Invalid input. Please enter a valid whole number.");
-      for (int i = 1; i <= 10; i++)
-         WriteLine ($"{num} * {i,2} = {num * i,-3}");
-      ReadLine ();
+   static void Main () {
+      for (int num = 1; num <= 10; num++) {
+         for (int i = 1; i <= 10; i++)
+            WriteLine ($"{num} * {i,2} = {num * i,-3}");
+         WriteLine ();
+      }
    }
 }
