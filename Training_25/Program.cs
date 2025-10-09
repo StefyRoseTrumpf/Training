952492917d05739d42c83c3104e8a034f4af92f7
stefyrose <AmalStefy.Regis@trumpf.com>
2025-10-09 10:45:08 +0530

    T07: Rectified

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,19 +3,14 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T07 branch. Assignment Q7: Pascal's Triangle.
+// Program prints the Pascal's triangle for the given number of rows.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training_25;
 
 internal class Program {
-   static void Main (string[] args) {
-      WriteLine ("Enter the number of rows for the Pascal's triangle:");
-      int rows;
-      while (true) {
-         if (int.TryParse (ReadLine (), out rows) && rows >= 0) break;
-         WriteLine ("Invalid input. Please enter a valid number (whole numbers).");
-      }
+   static void Main () {
+      int rows = ReadValidInt ("Enter the number of rows for the Pascal's triangle: ");
       for (int i = 0; i < rows; i++) {
          Write (new string (' ', (rows - i) * 2));
          int value = 1;
@@ -25,6 +20,17 @@ internal class Program {
          }
          WriteLine ();
       }
-      ReadLine ();
+   }
+
+   /// <summary>Repeadtedly prompts the user to enter a valid input.</summary>
+   static int ReadValidInt (string message) {
+      int num;
+      while (true) {
+         Write (message);
+         string? input = ReadLine ();
+         if (int.TryParse (input, out num) && num >= 0) break;
+         WriteLine ("Invalid input. Please enter a valid number(whole numbers).");
+      }
+      return num;
    }
 }
