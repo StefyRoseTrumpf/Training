cf68d2acd73bf21f7cffc75025133634a6b29afa
stefyrose <AmalStefy.Regis@trumpf.com>
2025-10-10 09:34:11 +0530

    T10: Modified

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,38 +3,29 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on T10 branch. Assignment Q10: Reverse the String
+// Program takes a string and returns the reversed string
 // ------------------------------------------------------------------------------------------------
 using System.Text;
 using static System.Console;
+
 namespace Training_25;
 
 internal class Program {
-   static void Main (string[] args) {
-      WriteLine ("Enter a string:");
-      string? input = ReadLine ();
-      string sb = StringReverser (input);
-      WriteLine ($"Input string: {input}\nReversed string: {sb}");
-
-      static string StringReverser (string? input) {
-         if (string.IsNullOrEmpty (input)) return string.Empty;
-         var letters = new StringBuilder ();
-         foreach (char c in input) {
-            if (c != ' ') letters.Append (c);
-         }
-         char[] reversedLetters = letters.ToString ().ToCharArray ();
-         Array.Reverse (reversedLetters);
-         var sb = new StringBuilder ();
-         int letterIndex = 0;
-         foreach (char c in input) {
-            if (c == ' ') sb.Append (' ');
-            else {
-               char ch = reversedLetters[letterIndex++];
-               sb.Append (char.IsUpper (c) ? char.ToUpper (ch) : char.ToLower (ch));
-            }
-         }
-         return sb.ToString ();
-      }
-      ReadLine ();
+   static void Main () {
+      Write ("Enter a string: ");
+      WriteLine ($"Reversed string: {StringReverser (ReadLine ())}");
+   }
+   static string StringReverser (string? input) {
+      if (string.IsNullOrEmpty (input)) return string.Empty;
+      var letters = new StringBuilder ();
+      foreach (char c in input) if (!char.IsWhiteSpace (c)) letters.Append (c);
+      var reversed = letters.ToString ().ToCharArray ();
+      Array.Reverse (reversed);
+      var sb = new StringBuilder ();
+      int i = 0;
+      foreach (char c in input)
+         sb.Append (char.IsWhiteSpace (c) ? c : char.IsUpper (c) ?
+            char.ToUpper (reversed[i++]) : char.ToLower (reversed[i++]));
+      return sb.ToString ();
    }
 }
