38a196b36f213fc2910c93afa4c83700d71deac8
stefyrose <AmalStefy.Regis@trumpf.com>
2025-10-29 12:40:00 +0530

    Test01: commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -9,28 +9,27 @@ using static System.Console;
 namespace Training_25;
 internal class Program {
    static void Main () {
-      GetValidInput();
+      string? input;
+      while (true) {
+         WriteLine ("Enter a string of numbers separated by comma: ");
+         input = ReadLine ();
+         if (string.IsNullOrEmpty (input) || input.Any (char.IsLetter)) {
+            WriteLine ("Input cannot be empty. Please enter a valid string of numbers.");
+            continue;
+         }
+         break;
+      }
+      WriteLine (SortEvenOdd (input));
    }
-   static void GetValidInput () {
-      WriteLine ("Enter a string of numbers separated by comma: ");
-      string? input = ReadLine();
-      if (string.IsNullOrEmpty (input)) {
-         WriteLine ("Input cannot be empty. Please enter a valid string of numbers.");
-         return;
-      }
-      else {
-         WriteLine (SortEvenOdd (input));
-      }
-      return;
 
-   }
+   // Function to sort even and odd numbers
    static int[] SortEvenOdd (string input) {
       string[] strArray = input.Split (',');
       int[] numArray = Array.ConvertAll (strArray, int.Parse);
       int[] evenArray = [.. numArray.Where (n => n % 2 == 0)];
-      Array.Sort(evenArray);
+      Array.Sort (evenArray);
       int[] oddArray = [.. numArray.Where (n => n % 2 != 0)];
-      Array.Sort(oddArray);
+      Array.Sort (oddArray);
       int[] sortedArray = [.. evenArray, .. oddArray];
       foreach (int num in sortedArray) {
          Write (num + " ");
