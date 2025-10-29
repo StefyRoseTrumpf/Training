ee09582dedab281bc069f0bc7e6d3389afa24541
stefyrose <AmalStefy.Regis@trumpf.com>
2025-10-29 10:54:39 +0530

    Test02: Initial commit

--- Training_25/Program.cs
+++ Training_25/Program.cs
@@ -3,11 +3,26 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program returns the alphabetic order corresponding to excel sheet for the given number.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training_25;
 internal class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+   static void Main () {
+      Write ("Enter a number: ");
+      int number = Convert.ToInt32 (ReadLine ());
+      string result = GetExcelColumnName (number);
+      WriteLine ($"{number} -> {result}");
+   }
+
+   // Function to convert a given number to its corresponding Excel column name.
+   static string GetExcelColumnName (int number) {
+      string columnName = string.Empty;
+      while (number > 0) {
+         int rem = (number - 1) % 26;
+         columnName = Convert.ToChar (rem + 'A') + columnName;
+         number = (number - 1) / 26;
+      }
+      return columnName;
    }
 }
